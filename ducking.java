408e57395ef9c92e1d177c6b79f0505d5fd45f0a
JeevithaR3 <jeevithajr2005@gmail.com>
2025-11-18 09:00:42 +0530

    ducking

---
+++ ducking.java
@@ -0,0 +1,19 @@
+public class ducking {
+    public static int divide(int a,int b)throws ArithmeticException 
+    {
+        return a/b;
+    }
+    public static void main(String args[])
+    {
+        try
+        {
+            int result=divide(10,0);
+            System.out.println("Result: "+result);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Exception handled in main:"+e);
+        }
+    }
+    
+}
